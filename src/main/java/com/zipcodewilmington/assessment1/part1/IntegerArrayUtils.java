29a0890002feb1e3d2e8db3abfa5a9a96d8d97d1
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-17 09:56:56 -0500

    IntegerArrayUtils coded, tested, compiles

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
@@ -9,7 +9,14 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray`
      */
     public static Integer getSum(Integer[] intArray) {
-        return null;
+        Integer sum = 0;
+
+        for(Integer element : intArray)
+        {
+            sum += element;
+        }
+
+        return sum;
     }
 
     /**
@@ -17,7 +24,14 @@ public class IntegerArrayUtils {
      * @return the product of `intArray`
      */
     public static Integer getProduct(Integer[] intArray) {
-        return null;
+        Integer product = 1;
+
+        for(Integer element : intArray)
+        {
+            product *= element;
+        }
+
+        return product;
     }
 
     /**
@@ -25,6 +39,10 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray` divided by number of elements in `intArray`
      */
     public static Double getAverage(Integer[] intArray) {
-        return null;
+        Integer sum = getSum(intArray);
+        Integer numberOfValues = intArray.length;
+        Double result = sum.doubleValue() / numberOfValues.doubleValue();
+
+        return result;
     }
 }
