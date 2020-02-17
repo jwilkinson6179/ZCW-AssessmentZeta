a576dd3d662956e01f469c64c156c81426fccb3e
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-17 09:38:21 -0500

    camelCase and reverse working in BasicStringUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,13 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        StringBuilder result = new StringBuilder(str.length());
+        String firstCharacterUpperCase = str.substring(0, 1).toUpperCase();
+        String restOfString = str.substring(1, str.length());
+
+        result.append(firstCharacterUpperCase).append(restOfString);
+
+        return result.toString();
     }
 
     /**
@@ -17,7 +23,14 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for(Integer i = str.length() - 1; i >= 0; i--)
+        {
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
     }
 
     /**
